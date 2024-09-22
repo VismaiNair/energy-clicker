f4dc6ac91a7dc5f9cbdfbdab332d0d4266191a3c
vismai.nair <vismai.nair@gmail.com>
2024-09-22 03:12:08 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add JS here
+const button = document.getElementById('energyButton');
+
+// Add event listener
+button.addEventListener('click', function() {
+  console.log('Button clicked!');
+});
